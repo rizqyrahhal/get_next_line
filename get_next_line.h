45e3b93dc5bcbd83f783f64299649eec2bb1840b
rizqyrahhal <rizqyrahhal8@gmail.com>
2021-12-22 19:41:07 +0100

    dsf

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,18 @@
 /*   By: rarahhal <rarahhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 20:17:14 by rarahhal          #+#    #+#             */
-/*   Updated: 2021/12/17 13:55:14 by rarahhal         ###   ########.fr       */
+/*   Updated: 2021/12/21 21:27:45 by rarahhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
 
-// # ifndef BUFFER_SIZE
-// # define BUFFER_SIZE 1000
-// # endif
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1000
+# endif
 
 char *get_next_line(int fd);
 
