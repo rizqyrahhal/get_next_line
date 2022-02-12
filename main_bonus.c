f9ff0f917886d1211aa88dfebb9dd4024a468369
rizqyrahhal <rizqyrahhal8@gmail.com>
2022-02-12 18:31:57 +0100

    joure de correction

--- main_bonus.c
+++ main_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rarahhal <rarahhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/12 18:27:22 by rarahhal          #+#    #+#             */
-/*   Updated: 2022/02/12 18:29:37 by rarahhal         ###   ########.fr       */
+/*   Created: 2022/02/12 18:27:09 by rarahhal          #+#    #+#             */
+/*   Updated: 2022/02/12 18:30:16 by rarahhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,24 @@ int	main()
 {
 	int fd = open("get_next_line.h", O_RDONLY);
 	char *line1;
+	// char *line2;
+	// char *line3;
+	// char *line4;
+	// char *line5;
+
+
+	int fd1 = open("Myfile.txt", O_RDONLY);
+	int fd2 = open("get_next_line.c", O_RDONLY);
 	line1 = get_next_line(fd);
 	printf("%s", line1);
 	free(line1);
-	line1 = get_next_line(fd);
+	line1 = get_next_line(fd1);
 	printf("%s", line1);
 	free(line1);
-	line1 = get_next_line(fd);
+	line1 = get_next_line(fd2);
 	printf("%s", line1);
 	free(line1);
-	line1 = get_next_line(fd);
+	line1 = get_next_line(fd1);
 	printf("%s", line1);
 	free(line1);
 	line1 = get_next_line(fd);
@@ -65,4 +73,27 @@ int	main()
 	return (0);
 }
 
-// cc -Wall -Wextra -Werror main.c get_next_line.c get_next_line.h get_next_line_utils.c -D BUFFER_SIZE=10 && ./a.out
+//OPEN_MAX :     Maximum number of files that one process can have open at anyone time.
+
+//OPEN_MAX : 	(65534) Maximum number of files that one process can have open at any given time
+
+/*Macro: int OPEN_MAX
+If defined, the unvarying maximum number of files that a single process can have open simultaneously.
+*/
+
+
+/*
+    {OPEN_MAX}
+        Maximum number of files that one process can have open at any one time.
+        Minimum Acceptable Value: {_POSIX_OPEN_MAX}
+*/
+
+
+
+
+
+
+
+// cc -Wall -Wextra -Werror main_bonus.c get_next_line_bonus.c get_next_line_bonus.h get_next_line_utils_bonus.c -D BUFFER_SIZE=10 && ./a.out
+// https://linoxide.com/install-debian-11-on-virtualbox/
+// https://velog.io/@yamkim/42-Seoul-Get-Next-Line2-Overview
